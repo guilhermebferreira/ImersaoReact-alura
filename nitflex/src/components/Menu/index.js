cc539079c594e093b303eede37568953a29ad5b7
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-07-27 21:58:22 -0300

    [UPDATE] adicionando barra de navegação, com a logo do projeto

--- nitflex/src/components/Menu/index.js
+++ nitflex/src/components/Menu/index.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Logo from '../../assets/img/logo.png'
+import './Menu.css'
 
 function Menu() {
     return(
-        <header>
-            Nitflex show!
-        </header>
+        <nav className="Menu">
+            <a href="/">
+            <img className="Logo" src={Logo} alt="AluraFlix - Imersão React Alura"/>
+            </a>
+        </nav>
     );
 
 }
